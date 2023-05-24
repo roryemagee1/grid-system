72c0105f95eca26438fe7c99a672c86ee0aaa0ca
Rory Magee <roryemagee@gmail.com>
2023-05-24 19:17:13 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+  
+
+
+  render() {
+    return (
+      <div className="App">
+        <main className="App-wrapper">
+          <h1> Test </h1>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
